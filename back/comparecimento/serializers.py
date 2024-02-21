d0601cc63beb413501c1c369d29d075985da6616
matheussASR <rm551466@fiap.com>
2024-02-21 00:53:36 -0300

    feat: finalizando backend

--- back/comparecimento/serializers.py
+++ back/comparecimento/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Comparecimento
+from naocomparecimento.models import NaoComparecimento
 
 class ComparecimentoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +11,6 @@ class ComparecimentoSerializer(serializers.ModelSerializer):
         """
         Valida se o email já existe no banco de dados.
         """
-        if Comparecimento.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Já existe um Comparecimento com este email.")
+        if Comparecimento.objects.filter(email=value).exists() or NaoComparecimento.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Já existe uma inscrição com este email.")
         return value
--- back/naocomparecimento/serializers.py
+++ back/naocomparecimento/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import NaoComparecimento
+from comparecimento.models import Comparecimento
 
 class NaoComparecimentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = NaoComparecimento
         fields = '__all__'
+
+    def validate_email(self, value):
+        """
+        Valida se o email já existe no banco de dados.
+        """
+        if Comparecimento.objects.filter(email=value).exists() or NaoComparecimento.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Já existe uma inscrição com este email.")
+        return value
